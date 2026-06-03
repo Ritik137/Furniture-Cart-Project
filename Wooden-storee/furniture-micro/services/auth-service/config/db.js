5f81f163daac7126904a4ceee2be2b24353b735f
Ritik Anand <anshritik1@gmail.com>
2026-06-03 16:07:24 +0530

    Remove node_modules and env files from tracking

--- Wooden-storee/furniture-micro/services/auth-service/config/db.js
+++ Wooden-storee/furniture-micro/services/auth-service/config/db.js
@@ -1,13 +1,13 @@
 import mongoose from 'mongoose';
 
-const connectDB = async() =>{
-    try{
+const connectDB = async () => {
+    try {
         await mongoose.connect(process.env.MONGO_URI);
         console.log("MongoDB Connected ✅");
 
-    } catch (error){
+    } catch (error) {
         console.error("MongoDB connection Error ❌");
-        process.exit(1);
+        console.error(error);
     }
 };
 
